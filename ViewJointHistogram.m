6a7b779cf77b60b5ac493b7a8db68896a6ed0e50
EGates1 <egates1@mdanderson.org>
2016-09-30 13:07:25 -0500

    Updated file
    Current issue: getting file names leaves the last file name with an unknown character on the end that makes it not read properly.  If you run through line 23 and examine filelist you should be able to see what I mean.

--- ViewJointHistogram.m
+++ ViewJointHistogram.m
@@ -1,6 +1,9 @@
 %% RegExpFiles - input regular expression
 %% OutputPNG   - Joint Histogram
 
+%%Current issue: getting file names leaves the last file name with an unknown character on the end that makes it not read properly.
+
+
 function ViewJointHistogram( OutputPNG, RegExpFiles)
 
 %% Load paths.
@@ -8,16 +11,45 @@ if ~isdeployed
   addpath('./nifti');
 end
 
+
+%In Terminal
 listfiles = ['ls ' RegExpFiles];
 disp(listfiles ); 
-[sysstatus,sysresult] = system(listfiles );
+[sysstatus,sysresult] = system(listfiles ); %last file has a strange character on the end
 
 %% TODO @EGates1 read each of these nifti files 
-%% TODO @EGates1 build joint histogram
-sysresult= strrep(sysresult,'\n','');
+
+sysresult= strrep(sysresult,'\n','');   %might not be needed, sysresult already space delimited?
 filelist = strsplit(sysresult,' ');
+
+images = cell(1, length(filelist) );
+maxdata=0; %maximum intensity accros all images, used to construct histogram bins
 
 for iii =1 :length(filelist)
    disp(['niifile = load_untouch_nii(''',filelist{iii} ,''');']);
-   %niifile = load_untouch_nii(filelist{iii});
+   niifile = load_untouch_nii(filelist{iii});
+   images{iii} = niifile.img;
+   maxdata = max(maxdata, max(niifile.img(:)) );
 end
+
+%% TODO @EGates1 build joint histogram
+
+%Histogram datasets
+nbins = 100;
+bins = linspace(0, maxdata, nbins); %start at 1 so 0 gets its own bin
+binned_data = zeros(nbins, length(filelist) );
+
+for iii =1 :length(filelist)
+    binned_data(:,iii) = hist(images{iii}(:), bins);
+end
+
+%% Plot (bar chart hard to read)
+figure(24601);
+plot(bins(2:end), binned_data(2:end,:)); %2:end excludes 0 bin, gives correct vertical scale
+title('Intensity Histograms');
+legend(filelist);
+
+saveas(gcf,OutputPNG); %set to save current figure
+
+
+
