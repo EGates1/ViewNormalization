9a6913725cde162da464822d4681aa00340a149a
EGates1 <egates1@mdanderson.org>
2016-10-03 11:05:12 -0500

    Updated file
    currently, data needs to be part of the current file path or else it won't read. That's the last workaround needed

--- ViewJointHistogram.m
+++ ViewJointHistogram.m
@@ -1,8 +1,6 @@
-%% RegExpFiles - input regular expression
-%% OutputPNG   - Joint Histogram
-
-%%Current issue: getting file names leaves the last file name with an unknown character on the end that makes it not read properly.
-
+%% ViewJointHistogram( OutputPNG, RegExpFiles) 
+%  RegExpFiles - input regular expression
+%  OutputPNG   - joint histogram name
 
 function ViewJointHistogram( OutputPNG, RegExpFiles)
 
@@ -11,19 +9,12 @@ if ~isdeployed
   addpath('./nifti');
 end
 
-
-%In Terminal
-listfiles = ['ls ' RegExpFiles];
-disp(listfiles ); 
-[sysstatus,sysresult] = system(listfiles ); %last file has a strange character on the end
+dirInfo=dir(RegExpFiles);
+filelist={dirInfo.name};
 
 %% TODO @EGates1 read each of these nifti files 
-
-sysresult= strrep(sysresult,'\n','');   %might not be needed, sysresult already space delimited?
-filelist = strsplit(sysresult,' ');
-
 images = cell(1, length(filelist) );
-maxdata=0; %maximum intensity accros all images, used to construct histogram bins
+maxdata=0;
 
 for iii =1 :length(filelist)
    disp(['niifile = load_untouch_nii(''',filelist{iii} ,''');']);
@@ -43,13 +34,12 @@ for iii =1 :length(filelist)
     binned_data(:,iii) = hist(images{iii}(:), bins);
 end
 
-%% Plot (bar chart hard to read)
+%% Plot
 figure(24601);
 plot(bins(2:end), binned_data(2:end,:)); %2:end excludes 0 bin, gives correct vertical scale
-title('Intensity Histograms');
-legend(filelist);
+title('Intensity Histograms (excludes 0)');
+xlabel('Intensity value')
+ylabel('Number of pixels')
+legend( strrep(filelist,'_','\_') ); %need to escape underscore characters
 
-saveas(gcf,OutputPNG); %set to save current figure
-
-
-
+saveas(gcf,OutputPNG, 'png'); %set to save current figure
